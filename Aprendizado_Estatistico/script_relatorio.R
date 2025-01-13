5b367b66c79d25f4e603a0602a8609e5e254e56a
unknown <nimerhammad98@gmail.com>
2025-01-13 20:18:50 -0300

    dados divididos

--- Aprendizado_Estatistico/script_relatorio.R
+++ Aprendizado_Estatistico/script_relatorio.R
@@ -4,3 +4,11 @@ library(MASS)
 dados = Boston
 #Explorar os dados
 summary(dados)
+
+#Dividin do os dados
+set.seed(42)
+install.package("caret")
+library(caret)
+train_index = createDataPartition(dados$medv, p = 0.75, list = FALSE)
+train_data = dados[train_index, ]
+test_data = dados[-train_index, ]
